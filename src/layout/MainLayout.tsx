7df9d22cbc8b3f990c84b8e9158cf5abc0c64174
AnkurDutta21 <daxxz2212@gmail.com>
2024-10-21 02:46:57 -0700

    added chatbot

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,15 +1,23 @@
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
+import { useState } from "react";
+import FAQChatbot from "../components/chatBot/FAQChatbot";
 
-const MainLayout = ({children}: {children: React.ReactNode}) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
       <div className="flex flex-col flex-grow">
         <Header />
-        <main className="flex-grow overflow-y-scroll p-4">
+        <main className="flex-grow overflow-y-scroll p-4 relative">
           {children}
+          <div className="fixed right-12 bottom-20 z-[999]">
+            {
+              isChatbotOpen ? <FAQChatbot setIsChatbotOpen={setIsChatbotOpen} /> : <img src="/assets/chatbot.png" alt="chatbot" className="w-10 h-10 cursor-pointer" onClick={() => setIsChatbotOpen(true)} />
+            }
+          </div>
         </main>
         <Footer />
       </div>
